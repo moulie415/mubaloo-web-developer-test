337f95c3be37d2a929d3150cdbfda5376fffa840
Henry Moule <hen10@hotmail.co.uk>
2019-10-19 19:15:01 +0100

    added form inputs and hooked up to post request

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './app.scss'
+import PropTypes from 'prop-types'
 
 const App = () => {
   const [username, setUsername] = useState()
@@ -19,19 +20,32 @@ const App = () => {
   return (
     <div>
       {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-      <form>
-        <FormInput label="Title:" id="title" />
-        <FormInput label="Date of birth:" id="dob" setVal={setDob} type="date" />
-
-        <label htmlFor="date-time">
-        Current date and time:
-          <input
-            type="datetime-local"
-            id="date-time"
-            name="date-time"
-          />
-        </label>
-      </form>
+      <div>
+        <FormInput label="Title:" id="title" setVal={val => setTitle(val)} />
+        <FormInput label="Name:" id="name" setVal={val => setName(val)} />
+        <FormInput label="Date of birth:" id="dob" setVal={val => setDob(val)} type="date" />
+      </div>
+      <div>
+        <FormInput label="Current location:" id="location" setVal={setLocation} />
+        <FormInput label="Current date and time:" id="dateTime" setVal={setDateTime} type="datetime-local" />
+        <FormInput label="User feedback:" id="feedback" setVal={setFeedback} />
+        <button
+          onClick={async () => {
+            console.log(title)
+            try {
+              await fetch('/api/submitForm', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({title, name, dob, location, dateTime, feedBack})
+              })
+            } catch (e) {
+              alert(e.message)
+            }
+          }}
+          type="button">
+        Submit
+        </button>
+      </div>
     </div>
   )
 }
@@ -43,10 +57,22 @@ const FormInput = ({id, label, type, setVal}) => {
       <input
         type={type}
         id={id}
-        onChange={event => setVal(event.target.value)}
+        onChange={(event) => {
+          setVal(event.target.value)
+        }}
       />
     </label>
   )
+}
+
+FormInput.defaultProps = {
+  type: 'text'
+}
+FormInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  setVal: PropTypes.func.isRequired
 }
 
 export default App
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const os = require('os');
+const express = require('express')
+const os = require('os')
 
-const app = express();
+const app = express()
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(express.static('dist'))
+app.use(express.json())
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+// fetch get username from os info and send back
+app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
+
+// log survey to console
+app.post('/api/submitForm', req => console.log(req.body))
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`))
